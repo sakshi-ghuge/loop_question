10a5e3864d3cc6b391bf6353f6adadb10a0e8877
sakshi-ghuge <sakshighuge24@navgurukul.org>
2025-08-23 15:22:29 +0530

    print world exactly n times

---
+++ 5.js
@@ -0,0 +1,8 @@
+// 5. Write a program that takes a positive integer n as input and prints "Hello World" exactly n times, each on a new line.
+
+let n = 10;
+let i = 1;
+while(i <= n){
+    console.log("Hello World");
+    i++;
+}
