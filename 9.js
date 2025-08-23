b350fce55b230c69fe1faab1fb2de03b4c54010e
sakshi-ghuge <sakshighuge24@navgurukul.org>
2025-08-23 16:14:38 +0530

    print the first n even numbers

---
+++ 9.js
@@ -0,0 +1,10 @@
+// 9. Write a program that takes a positive integer n as input and prints the first n even numbers starting from 2, separated by spaces.
+
+let n = 5;
+let i = 2;
+let c = 0;
+while(c < n){
+    console.log(i);
+    i += 2;
+    c++;
+}
