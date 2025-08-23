0d1b7431839b633ded4d5f2a7906993f0c6d625f
sakshi-ghuge <sakshighuge24@navgurukul.org>
2025-08-23 15:47:19 +0530

    print (*) on the same line

---
+++ 6.js
@@ -0,0 +1,8 @@
+// 6. Write a program that takes a positive integer n as input and prints n asterisks (*) on the same line, separated by spaces.
+
+let n = 5;
+let i = 1;
+while(i <= n){
+    process.stdout.write("* ");
+    i++;
+}
