9a7a4151bd887c9200a521c41f6220087522b219
sakshi-ghuge <sakshighuge24@navgurukul.org>
2025-08-23 18:05:01 +0530

    print the string word exactly n times

---
+++ 13.js
@@ -0,0 +1,9 @@
+// 13. Write a program that takes an integer n and a string word as input. Print the string word exactly n times on the same line with a space in between each, using a while loop.
+
+let n = 5;
+let i = 1;
+
+while(i <= n){
+    console.log("bye");
+    i++;
+}
