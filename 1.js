1ed44b7aa7206b71859e8c03733ca295acd56ed7
sakshi-ghuge <sakshighuge24@navgurukul.org>
2025-08-23 15:13:04 +0530

    print one to n

---
+++ 1.js
@@ -0,0 +1,9 @@
+// Problem Statement: 1
+// Write a program that takes a positive integer n as input and prints all numbers from 1 to n on the same line, separated by spaces.
+
+let N = 5;
+let i = 1;
+while(i <= 5){
+    console.log(i + " ");
+    i++;
+}
