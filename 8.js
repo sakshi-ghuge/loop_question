df07c76d629e5b64d391b6a2de5f18990c397432
sakshi-ghuge <sakshighuge24@navgurukul.org>
2025-08-23 16:13:27 +0530

    perfect square numbers less than or equal to n

---
+++ 8.js
@@ -0,0 +1,9 @@
+// 8. Write a program that takes a positive integer n as input and prints all perfect square numbers less than or equal to n, separated by spaces.
+
+let n = 30;
+let i = 1;
+
+while(i <= n){
+    console.log(i * i);
+    i++;
+}
