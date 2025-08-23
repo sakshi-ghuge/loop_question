def102ba2ef3282473d49fee934303a7b6c413da
sakshi-ghuge <sakshighuge24@navgurukul.org>
2025-08-23 16:15:45 +0530

    print the first n off numbers starting from 1

---
+++ 10.js
@@ -0,0 +1,11 @@
+// 10. Write a program that takes a positive integer n as input and prints the first n odd numbers starting from 1, separated by spaces.
+
+let n = 5;
+let i = 1;
+let c = 0;
+
+while(c < n){
+    console.log(i);
+    i += 2;
+    c++;
+}
