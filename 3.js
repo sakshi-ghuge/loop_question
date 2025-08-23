774ba55f23312e65cd53c8421e2dd9b77fda99fc
sakshi-ghuge <sakshighuge24@navgurukul.org>
2025-08-23 15:19:12 +0530

    print number n to 1

---
+++ 3.js
@@ -0,0 +1,8 @@
+// 3. Write a program that takes a positive integer n as input and prints numbers from n down to 1, separated by spaces.
+
+let n = 3;
+let i = n;
+while(i >= 1){
+    console.log(i + " ")
+    i--;
+}
